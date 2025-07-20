6965db00ff166ad73c4e8c485f020ee6855e3c45
pandora <indiscrete.void@gmail.com>
2025-07-20 21:05:04 +0300

    handle queue closes

--- src/Polysemy/Transport/Bus.hs
+++ src/Polysemy/Transport/Bus.hs
@@ -4,9 +4,9 @@ import Control.Concurrent.STM.TBMQueue
 import Data.List qualified as List
 import Polysemy
 import Polysemy.Conc (Race)
-import Polysemy.Conc.Effect.Lock
+import Polysemy.Conc.Effect.Queue (Queue)
+import Polysemy.Conc.Effect.Queue qualified as Effect.Queue
 import Polysemy.Conc.Interpreter.Queue.TBM
-import Polysemy.Conc.Queue
 import Polysemy.Conc.Queue qualified as Queue
 import Polysemy.Input
 import Polysemy.Output
@@ -30,8 +30,8 @@ recvdFrom addr = recvFrom addr . Queue.write
 
 type RecvFromTBMQueues addr i = [(addr, TBMQueue i)]
 
-stateGetTBMQueue :: (Member (Embed IO) r, Member (State (RecvFromTBMQueues addr i)) r, Member Lock r, Eq addr) => addr -> Sem r (TBMQueue i)
-stateGetTBMQueue addr = lock $ do
+stateGetTBMQueue :: (Member (Embed IO) r, Member (State (RecvFromTBMQueues addr i)) r, Eq addr) => addr -> Sem r (TBMQueue i)
+stateGetTBMQueue addr = do
   s <- State.get
   case List.find (\(xAddr, _) -> xAddr == addr) s of
     Just (_, queue) -> pure queue
@@ -40,12 +40,31 @@ stateGetTBMQueue addr = lock $ do
       State.put ((addr, queue) : s)
       pure queue
 
-interpretRecvFromTBMQueue :: (Member (Embed IO) r, Member Race r, Member Lock r, Eq addr) => InterpreterFor (RecvFrom addr i) r
+stateInterceptTBMQueueClose :: (Member (Queue i) r, Member (State (RecvFromTBMQueues addr i)) r, Eq addr) => addr -> Sem r a -> Sem r a
+stateInterceptTBMQueueClose addr m = do
+  intercept @(Queue _)
+    ( \case
+        Effect.Queue.Read -> Queue.read
+        Effect.Queue.TryRead -> Queue.tryRead
+        Effect.Queue.ReadTimeout t -> Queue.readTimeout t
+        Effect.Queue.Peek -> Queue.peek
+        Effect.Queue.TryPeek -> Queue.tryPeek
+        Effect.Queue.Write x -> Queue.write x
+        Effect.Queue.TryWrite x -> Queue.tryWrite x
+        Effect.Queue.WriteTimeout t x -> Queue.writeTimeout t x
+        Effect.Queue.Closed -> Queue.closed
+        Effect.Queue.Close -> do
+          State.modify $ List.filter (\(xAddr, _) -> xAddr /= addr)
+          Queue.close
+    )
+    m
+
+interpretRecvFromTBMQueue :: (Member (Embed IO) r, Member Race r, Eq addr) => InterpreterFor (RecvFrom addr i) r
 interpretRecvFromTBMQueue = fmap snd . stateToIO [] . runScopedNew go . raiseUnder @(State _)
   where
     go addr m = do
       queue <- stateGetTBMQueue addr
-      interpretQueueTBMWith queue m
+      interpretQueueTBMWith queue . stateInterceptTBMQueueClose addr $ m
 
 inputToQueue :: (Member (Queue i) r) => InterpreterFor (InputWithEOF i) r
 inputToQueue = interpret \case Input -> Queue.readMaybe
