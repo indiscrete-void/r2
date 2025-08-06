cda54b49aafb097d35e9e0e206b460648b8c89a8
iota <indiscrete.void@gmail.com>
2025-08-06 19:23:40 +0300

    don't test for RouteTo-RoutedFrom serialized equality

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,4 +1,3 @@
-import Data.Serialize
 import R2
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -10,11 +9,7 @@ testR2 =
   testGroup
     "r2"
     [ testCase "r2 SendTo node0 (RouteTo node1 msg) = SendTo node1 (RoutedFrom node0 msg)" $
-        r2 SendTo 0 (RouteTo 1 msg) @?= SendTo 1 (RoutedFrom 0 msg),
-      testCase "encode (RouteTo node (encode msg)) = encode (RouteTo node msg)" $
-        encode (RouteTo defaultAddr (encode msg)) @?= encode (RouteTo defaultAddr msg),
-      testCase "encode (RoutedFrom node (encode msg)) = encode (RoutedFrom node msg)" $
-        encode (RoutedFrom defaultAddr (encode msg)) @?= encode (RoutedFrom defaultAddr msg)
+        r2 SendTo 0 (RouteTo 1 msg) @?= SendTo 1 (RoutedFrom 0 msg)
     ]
   where
     msg = Just ()
