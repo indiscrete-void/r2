341160ed1f24f59df7625255c26c3c766f730925
pandora <indiscrete.void@gmail.com>
2025-08-29 04:19:44 +0300

    bus: use AtomicState

--- src/Polysemy/Transport/Bus.hs
+++ src/Polysemy/Transport/Bus.hs
@@ -3,6 +3,7 @@ module Polysemy.Transport.Bus (RecvFrom, SendTo, sendTo, recvdFrom, recvFrom, in
 import Control.Concurrent.STM.TBMQueue
 import Data.List qualified as List
 import Polysemy
+import Polysemy.AtomicState
 import Polysemy.Conc (Race)
 import Polysemy.Conc.Effect.Queue (Queue)
 import Polysemy.Conc.Effect.Queue qualified as Effect.Queue
@@ -11,8 +12,6 @@ import Polysemy.Conc.Queue qualified as Queue
 import Polysemy.Input
 import Polysemy.Output
 import Polysemy.Scoped
-import Polysemy.State
-import Polysemy.State qualified as State
 import Polysemy.Transport
 
 type RecvFrom addr i = Scoped addr (Queue i)
@@ -30,17 +29,17 @@ recvdFrom addr i = recvFrom addr $ Queue.write i
 
 type RecvFromTBMQueues addr i = [(addr, TBMQueue i)]
 
-stateGetTBMQueue :: (Member (Embed IO) r, Member (State (RecvFromTBMQueues addr i)) r, Eq addr) => addr -> Sem r (TBMQueue i)
+stateGetTBMQueue :: (Member (Embed IO) r, Member (AtomicState (RecvFromTBMQueues addr i)) r, Eq addr) => addr -> Sem r (TBMQueue i)
 stateGetTBMQueue addr = do
-  s <- State.get
+  s <- atomicGet
   case List.find (\(xAddr, _) -> xAddr == addr) s of
     Just (_, queue) -> pure queue
     Nothing -> do
       queue <- embed (newTBMQueueIO 1024)
-      State.put ((addr, queue) : s)
+      atomicModify' ((addr, queue) :)
       pure queue
 
-stateInterceptTBMQueueClose :: (Member (Queue i) r, Member (State (RecvFromTBMQueues addr i)) r, Eq addr) => addr -> Sem r a -> Sem r a
+stateInterceptTBMQueueClose :: (Member (Queue i) r, Member (AtomicState (RecvFromTBMQueues addr i)) r, Eq addr) => addr -> Sem r a -> Sem r a
 stateInterceptTBMQueueClose addr m = do
   intercept @(Queue _)
     ( \case
@@ -54,13 +53,13 @@ stateInterceptTBMQueueClose addr m = do
         Effect.Queue.WriteTimeout t x -> Queue.writeTimeout t x
         Effect.Queue.Closed -> Queue.closed
         Effect.Queue.Close -> do
-          State.modify $ List.filter (\(xAddr, _) -> xAddr /= addr)
+          atomicModify' $ List.filter (\(xAddr, _) -> xAddr /= addr)
           Queue.close
     )
     m
 
 interpretRecvFromTBMQueue :: (Member (Embed IO) r, Member Race r, Eq addr) => InterpreterFor (RecvFrom addr i) r
-interpretRecvFromTBMQueue = fmap snd . stateToIO [] . runScopedNew go . raiseUnder @(State _)
+interpretRecvFromTBMQueue = fmap snd . atomicStateToIO [] . runScopedNew go . raiseUnder @(AtomicState _)
   where
     go addr m = do
       queue <- stateGetTBMQueue addr
