a515ab0e625e1b7bc95d658c1f0f2af7fb573e52
iota <indiscrete.void@gmail.com>
2025-09-15 06:16:24 +0300

    r2d: extract handleR2OutputChan

--- src/R2/Daemon.hs
+++ src/R2/Daemon.hs
@@ -1,6 +1,7 @@
 module R2.Daemon (State, initialState, tunnelProcess, r2d) where
 
 import Control.Monad.Extra
+import Control.Monad.Loops (whileJust_)
 import Data.List qualified as List
 import Polysemy
 import Polysemy.Async
@@ -15,10 +16,10 @@ import Polysemy.Scoped
 import Polysemy.Trace
 import Polysemy.Transport
 import R2
-import R2.Peer
+import R2.Daemon.Bus
 import R2.Daemon.Sockets
 import R2.Daemon.Sockets.Accept
-import R2.Daemon.Bus
+import R2.Peer
 import System.Process.Extra
 import Text.Printf qualified as Text
 
@@ -139,11 +140,9 @@ exchangeSelves self maybeKnownAddr = do
     when (knownNodeAddr /= addr) $ fail (Text.printf "address mismatch")
   pure addr
 
-r2nd ::
+makeNode ::
   ( Member (AtomicState (State chan)) r,
     Member (Scoped CreateProcess Sem.Process) r,
-    Member (NodeBus Address chan Message) r,
-    Member (NodeBus NewConnection chan Message) r,
     Member (Bus chan Message) r,
     Members (Transport Message Message) r,
     Member Async r,
@@ -151,13 +150,31 @@ r2nd ::
     Member Fail r,
     Member Resource r
   ) =>
+  Address ->
   String ->
   Connection chan ->
   Sem r ()
-r2nd cmd conn@(Connection {..}) =
-  stateReflectNode conn $
-    traceTagged ("r2nd " <> show connAddr) $
-      handle (handleMsg cmd conn)
+makeNode self cmd conn@(Connection {..}) = do
+  async_ $
+    runNewNodeBus self cmd $
+      runR2NodeBus self cmd connAddr $
+        r2nd cmd conn
+
+handleR2OutputChan ::
+  ( Member (AtomicState (State chan)) r,
+    Member (Bus chan Message) r,
+    Member Fail r
+  ) =>
+  Address ->
+  chan ->
+  Address ->
+  Sem r ()
+handleR2OutputChan router chan addr = do
+  (Just routerNode) <- stateLookupNode router
+  let routerChan = nodeBusChan ToWorld (connChan routerNode)
+  whileJust_
+    (busChan chan takeChan)
+    (busChan routerChan . putChan . Just . MsgRouteTo . RouteTo addr)
 
 runR2NodeBus ::
   ( Member (AtomicState (State chan)) r,
@@ -179,48 +196,10 @@ runR2NodeBus self cmd router = interpret \case
       Just node -> pure $ connChan node
       Nothing -> do
         chan <- nodeBusMakeChan
-        async_ $ routeToChan (nodeBusChan ToWorld chan) addr
+        async_ $ handleR2OutputChan router (nodeBusChan ToWorld chan) addr
         let conn = Connection addr (R2 router) chan
         ioToNodeBusChan chan $ makeNode self cmd conn
         pure chan
-  where
-    routeToChan ::
-      ( Member (AtomicState (State chan)) r,
-        Member (Bus chan Message) r,
-        Member Fail r
-      ) =>
-      chan ->
-      Address ->
-      Sem r ()
-    routeToChan chan addr =
-      busChan chan $ do
-        (Just routerNode) <- stateLookupNode router
-        let routerChan = nodeBusChan ToWorld (connChan routerNode)
-        takeChan >>= \case
-          Just i -> do
-            busChan routerChan (putChan $ Just $ MsgRouteTo $ RouteTo addr i)
-              >> routeToChan chan addr
-          Nothing -> mempty
-
-makeNode ::
-  ( Member (AtomicState (State chan)) r,
-    Member (Scoped CreateProcess Sem.Process) r,
-    Member (Bus chan Message) r,
-    Members (Transport Message Message) r,
-    Member Async r,
-    Member Trace r,
-    Member Fail r,
-    Member Resource r
-  ) =>
-  Address ->
-  String ->
-  Connection chan ->
-  Sem r ()
-makeNode self cmd conn@(Connection {..}) = do
-  async_ $
-    runNewNodeBus self cmd $
-      runR2NodeBus self cmd connAddr $
-        r2nd cmd conn
 
 runNewNodeBus ::
   ( Member (Bus chan Message) r,
@@ -242,6 +221,26 @@ runNewNodeBus self cmd = interpret \case
       let conn = Connection addr newConnTransport chan
       makeNode self cmd conn
     pure chan
+
+r2nd ::
+  ( Member (AtomicState (State chan)) r,
+    Member (Scoped CreateProcess Sem.Process) r,
+    Member (NodeBus Address chan Message) r,
+    Member (NodeBus NewConnection chan Message) r,
+    Member (Bus chan Message) r,
+    Members (Transport Message Message) r,
+    Member Async r,
+    Member Trace r,
+    Member Fail r,
+    Member Resource r
+  ) =>
+  String ->
+  Connection chan ->
+  Sem r ()
+r2nd cmd conn@(Connection {..}) =
+  stateReflectNode conn $
+    traceTagged ("r2nd " <> show connAddr) $
+      handle (handleMsg cmd conn)
 
 r2sd ::
   ( Member (Accept sock) r,
