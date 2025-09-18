b35dbc5c0eff6610af13ffd5e83ec750eadfcc58
pandora <indiscrete.void@gmail.com>
2025-09-18 18:45:30 +0300

    don't trace I/O

--- src/R2/Peer.hs
+++ src/R2/Peer.hs
@@ -34,7 +34,6 @@ import Data.Aeson.TH
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as BC
 import Data.ByteString.Lazy qualified as LBS
-import Data.Functor
 import Data.Maybe
 import Data.Text qualified as Text
 import Debug.Trace qualified as Debug
@@ -45,9 +44,7 @@ import Options.Applicative
 import Polysemy
 import Polysemy.Async
 import Polysemy.Extra.Async
-import Polysemy.Extra.Trace
 import Polysemy.Fail
-import Polysemy.Trace
 import Polysemy.Transport
 import Polysemy.Transport.Extra
 import R2
@@ -155,51 +152,41 @@ address = str >>= maybeFail "invalid node ID" . parseAddressBase58
 
 runR2Input ::
   ( Member (InputWithEOF Message) r,
-    Member Fail r,
-    Member Trace r
+    Member Fail r
   ) =>
   Address ->
   InterpreterFor (InputWithEOF Message) r
-runR2Input node = traceTagged ("runR2Input " <> show node) . go . raiseUnder @Trace
-  where
-    go = interpret \case
-      Input ->
-        input >>= \case
-          Just (MsgRoutedFrom (RoutedFrom {..})) ->
-            if routedFromNode == node
-              then trace (show routedFromData) $> Just routedFromData
-              else fail $ "unexpected node: " <> show routedFromNode
-          Just msg -> fail $ "unexected message: " <> show msg
-          Nothing -> pure Nothing
+runR2Input node = interpret \case
+  Input ->
+    input >>= \case
+      Just (MsgRoutedFrom (RoutedFrom {..})) ->
+        if routedFromNode == node
+          then pure $ Just routedFromData
+          else fail $ "unexpected node: " <> show routedFromNode
+      Just msg -> fail $ "unexected message: " <> show msg
+      Nothing -> pure Nothing
 
 outputRouteTo :: (Member (Output Message) r) => Address -> Message -> Sem r ()
 outputRouteTo node = output . MsgRouteTo . RouteTo node
 
 runR2Close ::
   forall r.
-  ( Member (Output Message) r,
-    Member Trace r
+  ( Member (Output Message) r
   ) =>
   Address ->
   InterpreterFor Close r
-runR2Close node = traceTagged ("runR2Close " <> show node) . go . raiseUnder @Trace
-  where
-    go = interpret \case Close -> trace "closing" >> outputRouteTo node (MsgData Nothing)
+runR2Close node = interpret \case Close -> outputRouteTo node (MsgData Nothing)
 
 runR2Output ::
-  ( Member (Output Message) r,
-    Member Trace r
+  ( Member (Output Message) r
   ) =>
   Address ->
   InterpreterFor (Output Message) r
-runR2Output node = traceTagged ("runR2Output " <> show node) . go . raiseUnder @Trace
-  where
-    go = interpret \case Output msg -> trace (show msg) >> outputRouteTo node msg
+runR2Output node = interpret \case Output msg -> outputRouteTo node msg
 
 runR2 ::
   ( Members (Transport Message Message) r,
-    Member Fail r,
-    Member Trace r
+    Member Fail r
   ) =>
   Address ->
   InterpretersFor (Transport Message Message) r
@@ -208,40 +195,27 @@ runR2 node =
     . runR2Output node
     . runR2Input node
 
-runMsgInput :: (Member (InputWithEOF Message) r, Member Fail r, Member Trace r) => InterpreterFor (InputWithEOF Message) r
-runMsgInput = traceTagged "runMsgInput" . go . raiseUnder @Trace
-  where
-    go :: (Member (InputWithEOF Message) r, Member Fail r, Member Trace r) => InterpreterFor (InputWithEOF Message) r
-    go = interpret \case
-      Input ->
-        input >>= \case
-          Just (MsgData (Just raw)) -> do
-            let msg = decode @Message (LBS.fromStrict $ unRaw raw)
-            trace (show msg)
-            pure msg
-          Just (MsgData Nothing) -> pure Nothing
-          Just msg -> fail $ "unexected message: " <> show msg
-          Nothing -> pure Nothing
+runMsgInput :: (Member (InputWithEOF Message) r, Member Fail r) => InterpreterFor (InputWithEOF Message) r
+runMsgInput = interpret \case
+  Input ->
+    input >>= \case
+      Just (MsgData (Just raw)) -> pure $ decode @Message (LBS.fromStrict $ unRaw raw)
+      Just (MsgData Nothing) -> pure Nothing
+      Just msg -> fail $ "unexected message: " <> show msg
+      Nothing -> pure Nothing
 
-runMsgOutput :: (Member (Output Message) r, Member Trace r) => InterpreterFor (Output Message) r
-runMsgOutput = traceTagged "runMsgOutput" . go . raiseUnder @Trace
-  where
-    go :: (Member (Output Message) r, Member Trace r) => InterpreterFor (Output Message) r
-    go = interpret \case
-      Output msg -> trace (show msg) >> (output . MsgData . Just . Raw . LBS.toStrict . encode $ msg)
+runMsgOutput :: (Member (Output Message) r) => InterpreterFor (Output Message) r
+runMsgOutput = interpret \case
+  Output msg -> (output . MsgData . Just . Raw . LBS.toStrict . encode $ msg)
 
-runMsgClose :: (Member (Output Message) r, Member Trace r) => InterpreterFor Close r
-runMsgClose = traceTagged "runMsgClose" . go . raiseUnder @Trace
-  where
-    go :: (Member (Output Message) r, Member Trace r) => InterpreterFor Close r
-    go = interpret \case
-      Close -> trace "close" >> output (MsgData Nothing)
+runMsgClose :: (Member (Output Message) r) => InterpreterFor Close r
+runMsgClose = interpret \case
+  Close -> output (MsgData Nothing)
 
 msgToIO ::
   ( Member (InputWithEOF Message) r,
     Member (Output Message) r,
-    Member Fail r,
-    Member Trace r
+    Member Fail r
   ) =>
   InterpretersFor (Transport Message Message) r
 msgToIO =
@@ -255,14 +229,13 @@ ioToMsg ::
     Member (InputWithEOF ByteString) r,
     Member (Output ByteString) r,
     Member Close r,
-    Member Async r,
-    Member Trace r
+    Member Async r
   ) =>
   Sem r ()
 ioToMsg =
   sequenceConcurrently_
-    [ traceTagged "msgToIn" $ contramapInput (>>= fmap unRaw . msgData) inputToOutput >> close,
-      traceTagged "outToMsg" $ mapOutput (MsgData . Just . Raw) inputToOutput >> output (MsgData Nothing)
+    [ contramapInput (>>= fmap unRaw . msgData) inputToOutput >> close,
+      mapOutput (MsgData . Just . Raw) inputToOutput >> output (MsgData Nothing)
     ]
 
 ioToR2 ::
@@ -272,7 +245,6 @@ ioToR2 ::
     Member (Output ByteString) r,
     Member Fail r,
     Member Close r,
-    Member Trace r,
     Member Async r
   ) =>
   Address ->
