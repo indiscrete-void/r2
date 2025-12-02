ca91bf3e7f59f0427ebc7c720fbf57728c0c0172
iota <indiscrete.void@gmail.com>
2025-12-02 03:11:11 +0300

    move handleR2Msg to general R2.Peer

--- src/R2/Daemon/Handler.hs
+++ src/R2/Daemon/Handler.hs
@@ -1,6 +1,5 @@
 module R2.Daemon.Handler (StatelessConnection (..), EstablishedConnection (..), tunnelProcess, listNodes, connectNode, routeTo, routedFrom, handleMsg) where
 
-import Control.Monad.Extra
 import Data.Maybe
 import Polysemy
 import Polysemy.Async
@@ -17,10 +16,6 @@ import R2.Daemon.MakeNode
 import R2.Daemon.Node
 import R2.Peer
 import System.Process.Extra
-
-newtype StatelessConnection = StatelessConnection Address
-
-newtype EstablishedConnection = EstablishedConnection Address
 
 tunnelProcess ::
   ( Member (Scoped CreateProcess Process) r,
@@ -48,36 +43,6 @@ connectNode ::
 connectNode router transport maybeNewNodeID = do
   chan <- makeAcceptedNode maybeNewNodeID (Pipe router transport)
   msgToIO $ runSerialization $ nodeBusChanToIO chan
-
-routeTo :: (Member (LookupChan EstablishedConnection (Maybe chan)) r, Member (Bus chan Message) r, Member (Output Message) r) => Address -> RouteTo Message -> Sem r ()
-routeTo = r2 \routeToAddr routedFrom -> do
-  mChan <- lookupChan ToWorld (EstablishedConnection routeToAddr)
-  case mChan of
-    Just chan -> busChan chan $ putChan (Just $ MsgR2 $ MsgRoutedFrom routedFrom)
-    Nothing -> output $ MsgR2 $ MsgRouteToErr routeToAddr "unreachable"
-
-routeToError :: (Member (LookupChan EstablishedConnection (Maybe chan)) r, Member (Bus chan Message) r) => Address -> String -> Sem r ()
-routeToError addr _ = do
-  mChan <- lookupChan ToWorld (EstablishedConnection addr)
-  whenJust mChan \chan -> busChan chan (putChan Nothing)
-
-routedFrom :: (Member (LookupChan StatelessConnection chan) r, Member (Bus chan Message) r) => RoutedFrom Message -> Sem r ()
-routedFrom (RoutedFrom routedFromNode routedFromData) = do
-  chan <- lookupChan FromWorld (StatelessConnection routedFromNode)
-  busChan chan $ putChan (Just routedFromData)
-
-handleR2Msg ::
-  ( Member (LookupChan EstablishedConnection (Maybe chan)) r,
-    Member (LookupChan StatelessConnection chan) r,
-    Member (Bus chan Message) r,
-    Member (Output Message) r
-  ) =>
-  Address ->
-  R2Message Message ->
-  Sem r ()
-handleR2Msg connAddr (MsgRouteTo msg) = routeTo connAddr msg
-handleR2Msg _ (MsgRouteToErr addr err) = routeToError addr err
-handleR2Msg _ (MsgRoutedFrom msg) = routedFrom msg
 
 handleMsg ::
   ( Member (Reader [Node chan]) r,
