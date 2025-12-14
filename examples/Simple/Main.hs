90992714a4f5e067da8c29aae8f317038cdddba2
pandora <indiscrete.void@gmail.com>
2025-12-14 11:55:07 +0300

    make DSL for defining in-memory networks

---
+++ examples/Simple/Main.hs
@@ -0,0 +1,86 @@
+import Data.ByteString.Char8 qualified as BC
+import Options.Applicative
+import Polysemy
+import Polysemy.Trace
+import Polysemy.Transport
+import R2.Client
+import R2.DSL
+import R2.Options
+import R2.Peer (ProcessTransport (Stdio))
+
+newtype Options = Options Verbosity
+
+parse :: IO Options
+parse = execParser parserInfo
+
+parserInfo :: ParserInfo Options
+parserInfo =
+  info
+    (helper <*> opts)
+    ( fullDesc
+        <> progDesc "DSL demo"
+        <> header "Route-to network"
+    )
+
+opts :: Parser Options
+opts = Options <$> verbosity
+
+pana :: NetworkNode
+pana = node "p4n4"
+
+bob :: NetworkNode
+bob = node "bQb"
+
+carl :: NetworkNode
+carl = node "k4r1"
+
+catnet :: (Members (NetworkEffects msgChan stdioChan) r) => Sem r ()
+catnet = do
+  Network {conn, conn_} <-
+    mkNet $
+      static
+        { serve =
+            [ (bob, exec "cat")
+            ],
+          link =
+            [ (pana, carl),
+              (carl, bob)
+            ]
+        }
+
+  conn_ (pana :/> RouteNil) Ls
+  conn_ (pana :/> carl :/> RouteNil) Ls
+  conn_ (pana :/> carl :/> bob :/> RouteNil) Ls
+
+  let panaMsgViaCarl = "pana greets bob"
+  bobRes <- conn (pana :/> carl :/> bob :/> RouteNil) (Tunnel Stdio) $ do
+    output (BC.pack panaMsgViaCarl)
+    echo <- BC.unpack <$> inputOrFail
+    close
+    pure echo
+  trace bobRes
+
+main :: IO ()
+main = do
+  Options verbosity <- parse
+  dslToIO verbosity catnet
+
+{-
+panaDaemon = do
+  Network {conn} <-
+    (mkDaemon pana)
+      daemonConfig
+        { peers =
+            [ socat carl "tcp:example.com:47210",
+              carl :/> bob
+            ]
+        }
+
+  let panaMsgViaCarl = "pana greets bob"
+  bobRes <- conn bob $ printf "echo %s" panaMsgViaCarl
+  assertEq bobRes panaMsgViaCarl
+
+simpleDaemon = await =<< mkDaemon me daemonConfig {peers = [carl]}
+  where
+    me = node "pana"
+-}
