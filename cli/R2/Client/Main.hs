8aaf1b004a269a9123846587022a41fe27ac6ec8
pandora <indiscrete.void@gmail.com>
2025-12-19 05:52:09 +0300

    fix client socket being doubled

--- cli/R2/Client/Main.hs
+++ cli/R2/Client/Main.hs
@@ -1,11 +1,7 @@
-import Network.Socket hiding (close)
 import R2.Client
 import R2.Client.Options
-import R2.Peer
 
 main :: IO ()
 main = do
   (Options verbosity command maybeSocketPath) <- parse
-  withR2Socket \s -> do
-    connect s =<< r2SocketAddr maybeSocketPath
-    r2cIO verbosity Nothing maybeSocketPath command
+  r2cIO verbosity Nothing maybeSocketPath command
