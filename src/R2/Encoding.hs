5a786d2077ccd4bf089d442774d393aaebb42963
iota <indiscrete.void@gmail.com>
2026-01-22 07:49:42 +0300

    move polysemy-transport serialization here

--- src/R2/Encoding.hs
+++ src/R2/Encoding.hs
@@ -9,6 +9,8 @@ import Data.ByteString.Char8 qualified as BC
 import GHC.Generics
 import Polysemy
 import Polysemy.Fail
+import R2.Encoding.LengthPrefix
+import Polysemy.Transport
 
 encodeStrict :: (ToJSON a) => a -> StrictByteString
 encodeStrict = B.toStrict . encode
@@ -40,3 +42,39 @@ decodeStrictSem = eitherToFail . eitherDecodeStrict
 
 decodeBase64Sem :: (Member Fail r, FromJSON a) => Base64Text -> Sem r a
 decodeBase64Sem = eitherToFail . decodeBase64
+
+deserializeInput :: forall a r. (FromJSON a, Member Fail r, Member ByteInputWithEOF r) => InterpreterFor (InputWithEOF a) r
+deserializeInput =
+  lenDecodeInput
+    . reinterpret \case
+      Input -> do
+        mbs <- input
+        case mbs of
+          Just bs -> do
+            i <- either fail pure $ eitherDecodeStrict bs
+            pure (Just i)
+          Nothing -> pure Nothing
+
+deserializedInput :: (FromJSON i, Member Fail r) => Sem (InputWithEOF i ': r) a -> Sem (ByteInputWithEOF ': r) a
+deserializedInput = deserializeInput . raiseUnder
+
+serializeOutput :: forall a r. (ToJSON a, Member ByteOutput r) => InterpreterFor (Output a) r
+serializeOutput =
+  lenPrefixOutput . reinterpret \case
+    Output o -> output (encodeStrict o)
+
+serializedOutput :: (ToJSON o) => Sem (Output o ': r) a -> Sem (ByteOutput ': r) a
+serializedOutput = serializeOutput . raiseUnder
+
+runSerialization ::
+  ( Member ByteInputWithEOF r,
+    Member ByteOutput r,
+    Member Fail r,
+    FromJSON i,
+    ToJSON o
+  ) =>
+  InterpretersFor '[InputWithEOF i, Output o] r
+runSerialization = serializeOutput . deserializeInput
+
+serialized :: (FromJSON i, ToJSON o, Member Fail r) => Sem (InputWithEOF i ': Output o ': r) a -> Sem (ByteInputWithEOF ': ByteOutput ': r) a
+serialized = runSerialization . raise2Under @ByteInputWithEOF . raise2Under @ByteOutput
---
+++ src/R2/Encoding/LengthPrefix.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module R2.Encoding.LengthPrefix
+  ( binaryDecodeStrict,
+    lenDecodeInput,
+    lenPrefixOutput,
+  )
+where
+
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
+import Data.Binary (Binary)
+import Data.Binary qualified as Binary
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as B
+import Polysemy hiding (send)
+import Polysemy.Output
+import Polysemy.State (State)
+import Polysemy.State qualified as State
+import Polysemy.Transport
+
+binaryDecodeStrict :: (Binary a) => ByteString -> Maybe a
+binaryDecodeStrict b = do
+  case Binary.decodeOrFail $ B.fromStrict b of
+    Right ("", _, a) -> Just a
+    _ -> Nothing
+
+binaryEncodeStrict :: (Binary a) => a -> ByteString
+binaryEncodeStrict = B.toStrict . Binary.encode
+
+inputOrGet ::
+  forall a r.
+  ( Member (State (Maybe a)) r,
+    Member (InputWithEOF a) r
+  ) =>
+  Sem r (Maybe a)
+inputOrGet =
+  State.get >>= \case
+    Just a -> State.put @(Maybe a) Nothing >> pure (Just a)
+    Nothing -> input
+
+inputN ::
+  ( Member (State (Maybe ByteString)) r,
+    Member (InputWithEOF ByteString) r
+  ) =>
+  Int ->
+  Sem r (Maybe ByteString)
+inputN n = runMaybeT $ MaybeT inputOrGet >>= go
+  where
+    go b
+      | B.length b >= n = finish b
+      | otherwise = retry b
+
+    retry leftover =
+      MaybeT inputOrGet >>= \new ->
+        let cumulative = leftover <> new
+         in go cumulative
+
+    finish accumulated =
+      let (result, rest) = B.splitAt n accumulated
+       in result <$ lift (State.put (Just rest))
+
+lenDecodeInput :: (Member ByteInputWithEOF r) => InterpreterFor ByteInputWithEOF r
+lenDecodeInput = State.evalState (Nothing :: Maybe ByteString) . go . raiseUnder
+  where
+    go :: (Member ByteInputWithEOF r, Member (State (Maybe ByteString)) r) => InterpreterFor ByteInputWithEOF r
+    go = interpret \case
+      Input -> runMaybeT do
+        sizeRaw <- MaybeT (inputN 8)
+        size <- hoistMaybe $ binaryDecodeStrict sizeRaw
+        MaybeT $ inputN size
+
+lenPrefixOutput :: (Member ByteOutput r) => InterpreterFor ByteOutput r
+lenPrefixOutput = interpret \case
+  Output o -> do
+    let size = binaryEncodeStrict $ B.length o
+    output $ size <> o
+
